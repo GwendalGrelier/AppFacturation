3213078dba4d948a865b94104e9fef68b3484e8f
GwendalGrelier <grelier.gwendal@hotmail.fr>
2020-01-13 16:07:21 +0100

    fixed database connexion

--- Models/Model.php
+++ Models/Model.php
@@ -4,11 +4,15 @@
 abstract class Model
 {
 
-    const SERVER = 'sqlprive-pc2372-001.privatesql.ha.ovh.net:3306';
-    const USER = 'cefiidev957';
-    const PASSWORD = '4iC9Ze6t';
-    const BASE = 'cefiidev957';
+    // const SERVER = 'sqlprive-pc2372-001.privatesql.ha.ovh.net:3306';
+    // const USER = 'cefiidev957';
+    // const PASSWORD = '4iC9Ze6t';
+    // const BASE = 'cefiidev957';
     
+    const SERVER = 'localhost';
+    const USER = 'root';
+    const PASSWORD = '';
+    const BASE = 'appfactures';
 
     protected $connexion;
 
@@ -20,8 +24,23 @@ abstract class Model
             $this->connexion->exec("SET NAMES 'UTF8'");
         } catch (Exception $e) {
             echo 'Erreur : ' . $e->getMessage();
+            echo $e->getCode();
         }
     }
+
+    /**
+     * Gets the list of every Quote
+     *
+     * @return array
+     */
+    public function getDevisList()
+       {
+           $request = "SELECT * FROM devis";
+           $request = $this->connexion->query($request);
+           $devisList = $request->fetchAll(PDO::FETCH_ASSOC);
+           
+           return $devisList;
+       }
 
    
 }
